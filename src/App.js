fd575770460d4eae62feaf5ab271d6e0d8257466
WIN-E8402T24THH\Nivrep <oruczade1229@gmail.com>
2023-04-07 18:35:36 +0400

    API - component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useMemo, useState, useRef } from "react";
+import React, { useMemo, useState, useRef, useEffect } from "react";
 import PostList from "./components/PostList";
 import "./styles/App.scss";
 import PostForm from "./components/PostForm";
@@ -6,6 +6,9 @@ import PostFilter from "./components/PostFilter";
 import MyModal from "./components/UI/MyModal/MyModal";
 import MyButton from "./components/UI/button/MyButton";
 import { usePosts } from "./hooks/usePosts";
+import axios from "axios";
+import PostService from "./API/PostService";
+
 function App() {
   const [posts, setPosts] = useState([]);
 
@@ -15,17 +18,34 @@ function App() {
 
   const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
 
+  /*  Post əlavə etmək üçün  */
+
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
     setModal(false);
   };
 
+  /*  Postu silmək üçün  */
+
   const removePost = (post) => {
     setPosts(posts.filter((p) => p.id !== post.id));
   };
 
+  /*  Axios  */
+
+  async function fetchPosts() {
+    const posts = await PostService.getAll();
+    setPosts(posts);
+  }
+
+  /*  UseEffecT  */
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
   return (
     <div className="App">
+      <MyButton onClick={fetchPosts}>Get Posts</MyButton>
       <MyButton style={{ marginTop: 30 }} onClick={() => setModal(true)}>
         создать пользователя
       </MyButton>
